761d60487f08c1c105088b8852a2546764ef5d06
jaseg <s@jaseg.de>
2012-01-30 23:15:57 +0100

    Starting to flesh things out...

---
+++ sinatra/communicator.rb
@@ -0,0 +1,9 @@
+
+require 'rubygems'
+require 'serialport'
+
+sp = SerialPort.new("/dev/ttyACM0", 115200)
+
+def set_frame (frameData)
+
+end
--- sinatra/server.rb
+++ sinatra/server.rb
@@ -1,1 +1,41 @@
 #!/usr/bin/env ruby
+
+require 'rubygems'
+require 'sinatra'
+require 'mongo'
+require 'json'
+require 'communicator'
+
+get '/lamps/:id' do |lamp|
+
+end
+
+post '/lamps/:id' do |lamp|
+    
+end
+
+
+get '/switches/:id' do |switch|
+
+end
+
+
+get '/switches/:id/handlers' do |switch|
+
+end
+
+post '/switches/:id/handlers' do |switch|
+
+end
+
+delete '/switches/:id/handlers/:handler' do |switch, handler|
+
+end
+
+get '/meters/:id' do |meter|
+
+end
+
+post '/meters/:id' do |meter|
+
+end
