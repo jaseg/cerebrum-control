e05aa35321ceca161f1ce06970043d994ad8a13d
jaseg <s@jaseg.de>
2012-02-02 23:49:13 +0100

    linking the stuff, completing the driver scaffold

--- sinatra/communicator.rb
+++ sinatra/communicator.rb
@@ -1,9 +1,60 @@
+=begin
+This is the actual driver. The arduino's serial port is opened here at startup and kept open since the arduino will reset every time the serial port is reconnected (that being caused by some rs232 transmission status line/linux kernel fnord).
+The lamp status is cached in a buffer and updated FRAME_RATE times per second.
+TODO
+This code is not (yet) thread-safe, but in case it is wanted to be... well, some mutexes should do it.
+The device id for the serial port is guessed (currently).
+=end
 
 require 'rubygems'
 require 'serialport'
 
-sp = SerialPort.new("/dev/ttyACM0", 115200)
+#possibly 20fps is a bit too much here...
+FRAME_RATE = 20 unless FRAME_RATE
 
-def set_frame (frameData)
+class Communicator
 
+  def initialize (port)
+    @sp = SerialPort.new(port, 115200)
+    @frame_buffer = Array.new()
+    Thread.start do
+      while true
+        send_frame
+        poll_switches
+        sleep 1.0/FRAME_RATE
+      end
+    end
+  end
+
+  def set_frame (frameData)
+    @frame_buffer = frameData
+  end
+
+  def set_lamp (id, value)
+    @frame_buffer[id] = value
+  end
+
+  def get_lamp (id)
+    @frame_buffer[id]
+  end
+
+  def set_lamps (buffer)
+    @frame_buffer = buffer
+  end
+
+  def get_lamps ()
+    @frame_buffer
+  end
+
+  def send_frame ()
+
+  end
+
+  def poll_switches ()
+
+  end
+
+  def set_meter(id, value)
+    send_command ("m" id value)
+  end
 end
--- sinatra/server.rb
+++ sinatra/server.rb
@@ -6,36 +6,41 @@ require 'mongo'
 require 'json'
 require 'communicator'
 
-get '/lamps/:id' do |lamp|
+enable :lock
+outcom = Communicator.new("/dev/ttyACM0")
 
+get '/lamps' do
+  outcom.get_lamps
+end
+
+post '/lamps' do
+  outcom.set_lamps (params[:buffer])
+end
+
+get '/lamps/:id' do |lamp|
+  outcom.get_lamp (lamp)
 end
 
 post '/lamps/:id' do |lamp|
-    
+  outcom.set_lamp (lamp, params[:state])
 end
 
 
 get '/switches/:id' do |switch|
-
+  '{state: 0}'
 end
 
-
 get '/switches/:id/handlers' do |switch|
-
+  '[]'
 end
 
 post '/switches/:id/handlers' do |switch|
-
 end
 
 delete '/switches/:id/handlers/:handler' do |switch, handler|
-
 end
 
-get '/meters/:id' do |meter|
-
-end
 
 post '/meters/:id' do |meter|
-
+  outcom.set_meter (meter, params[:value])
 end
